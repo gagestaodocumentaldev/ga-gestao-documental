d689d4690ad4c5b452e6e8f023e8b6a6de88bd94
conuaz <lucas.olasz@basisti.com.br>
2026-06-24 15:08:33 -0300

    Inclui configuracoes do next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "localhost:3000",
+    "127.0.0.1:3000",
+    "10.34.6.112:3000", // IP da sua máquina na rede
+  ],
 };
 
 export default nextConfig;
