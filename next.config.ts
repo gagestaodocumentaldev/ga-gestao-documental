1929c5d77372c1c0fa7b44b349ce72529ba6240e
lucasolasz <olaszlucas072@gmail.com>
2026-06-26 00:07:09 -0300

    Corrige configuracao de ips permitidos

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    "localhost:3000",
-    "127.0.0.1:3000",
-    "10.34.6.112:3000",
-    "192.168.1.33:3000", // IP da sua máquina na rede
+    "localhost",
+    "127.0.0.1",
+    "10.34.6.112",
+    "192.168.1.33", // IP da sua máquina na rede
   ],
 };
 
