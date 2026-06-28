70c6f0c8291e11f6404462ce2aaef69c7b7ca08b
lucasolasz <olaszlucas072@gmail.com>
2026-06-28 13:51:04 -0300

    Correcao do sql de policies

--- migrations/create_familias_documentos.sql
+++ migrations/create_familias_documentos.sql
@@ -14,16 +14,18 @@ using (true);
 
 create policy "authenticated users can insert familias_documentos"
 on "public"."familias_documentos"
-to INSERT
+for INSERT
+to authenticated
 with check (true);
 
 create policy "authenticated users can delete familias_documentos"
 on "public"."familias_documentos"
-to DELETE
-using (true)
-with check (true);
+for DELETE
+to authenticated
+using (true);
 
 create policy "authenticated users can select familias_documentos"
 on "public"."familias_documentos"
-to SELECT
+for SELECT
+to authenticated
 using (true);
